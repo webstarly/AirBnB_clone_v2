ad1820e01d647dcdff55c63cca67ec73af8682ac
Edidiong Ukem <edidionge.ukem@gmail.com>
2023-04-02 21:54:18 -0700

    Airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,19 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+import models
+from os import getenv
 
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True, nullable=False, unique=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -35,10 +44,22 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
+       # dictionary = {}
+       # dictionary.update(self.__dict__)
+       # dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+       # dictionary['created_at'] = self.created_at.isoformat()
+       # dictionary['updated_at'] = self.updated_at.isoformat()
+       # return dictionary
+        model_dict = self.__dict__.copy()
+        model_dict['__class__'] = type(self).__name__
+        model_dict['created_at'] = self.created_at.isoformat()
+        model_dict['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in model_dict.keys():
+            del model_dict['_sa_instance_state']
+        return model_dict
+
+    def delete(self):
+        """delete the current instance from the storage """
+        models.storage.delete(self)
+
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,7 +48,7 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-     def delete(self, obj=None):
+    def delete(self, obj=None):
         """ delete obj from __objects"""
         for key, value in FileStorage.__objects.items():
             if obj is value:
