7e67e23c1cee3a1bd799b4dc572182962d563097
Edidiong Ukem <edidionge.ukem@gmail.com>
2023-04-06 18:43:42 -0700

    clone_v2

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,75 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from os import getenv
+from models.base_model import BaseModel,Base
+from models.place import place_amenity
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    '''
+        Implementation for the Amenities.
+    '''
+    __tablename__ = "amenities"
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity,
+                                       back_populates="amenities")
+    else:
+        name = ""
+# from os import getenv
+# from models.base_model import BaseModel, Base
+# from models.place import place_amenity
+# from sqlalchemy import Column, String, Integer
+# from sqlalchemy import Float, ForeignKey
+# from sqlalchemy.orm import relationship
+# from models.amenity import Amenity
+# from models.review import Review
+# import models import storage
+
+
+# class Amenity(BaseModel, Base):
+#     '''
+#         Implementation for the Amenities.
+#     '''
+#     __tablename__ = "places"
+#     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+#     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+#     name = Column(String(128), nullable=False)
+#     description = Column(String(1024))
+#     number_rooms = Column(Integer, default=0)
+#     number_bathrooms = Column(Integer, default=0)
+#     max_guest = Column(Integer, default=0)
+#     price_by_night = Column(Integer, default=0)
+#     latitude = Column(Float)
+#     longitude = Column(Float)
+#     reviews = relationship("Review", backref="place", cascade="delete")
+#     amenities = relationship("Amenity", secondary="place_amenity",
+#                              viewonly=False)
+#     amenity_ids = []
+
+#     if getenv("HBNB_TYPE_STORAGE", None) != "db":
+#         @property
+#         def reviews(self):
+#             """Get a list of all linked Reviews."""
+#             review_list = []
+#             for review in list(models.storage.all(Review).values()):
+#                 if review.place_id == self.id:
+#                     review_list.append(review)
+#             return review_list
+
+#         @property
+#         def amenities(self):
+#             """Get/set linked Amenities."""
+#             amenity_list = []
+#             for amenity in list(models.storage.all(Amenity).values()):
+#                 if amenity.id in self.amenity_ids:
+#                     amenity_list.append(amenity)
+#             return amenity_list
+
+#         @amenities.setter
+#         def amenities(self, value):
+#             if type(value) == Amenity:
+#                 self.amenity_ids.append(value.id)
+
--- models/base_model.py
+++ models/base_model.py
@@ -9,48 +9,57 @@ from os import getenv
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True, nullable=False, unique=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-
+    
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            if kwargs:
+                for key, value in kwargs.items():
+                    if key == "__class__":
+                        pass
+                    elif key == "updated_at" or key == "created_at":
+                        setattr(self, key, datetime.strptime(
+                                value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
+        # cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        # return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-       # dictionary = {}
-       # dictionary.update(self.__dict__)
-       # dictionary.update({'__class__':
-       #                   (str(type(self)).split('.')[-1]).split('\'')[0]})
-       # dictionary['created_at'] = self.created_at.isoformat()
-       # dictionary['updated_at'] = self.updated_at.isoformat()
-       # return dictionary
+                
+        # dictionary = {}
+        # dictionary.update(self.__dict__)
+        # dictionary.update({'__class__':
+        #                   (str(type(self)).split('.')[-1]).split('\'')[0]})
+        # dictionary['created_at'] = self.created_at.isoformat()
+        # dictionary['updated_at'] = self.updated_at.isoformat()
+        # for key, value in dictionary.items():
+        #     if key == "_sa_instance_state":
+        #         del dictionary["_sa_instance_state"]
+        # return dictionary
         model_dict = self.__dict__.copy()
         model_dict['__class__'] = type(self).__name__
         model_dict['created_at'] = self.created_at.isoformat()
@@ -62,4 +71,3 @@ class BaseModel:
     def delete(self):
         """delete the current instance from the storage """
         models.storage.delete(self)
-
