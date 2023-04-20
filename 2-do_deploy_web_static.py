b3cb3e6be63acad81ae897c235f17851c37000df
webstarly <edidionge.ukem@gmail.com>
2023-04-20 09:44:21 +0000

    clone_v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,54 @@
 #!/usr/bin/python3
+"""a Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to web servers,
+using the function do_deploy:
 """
-A Fabric script (based on the file 1-pack_web_static.py
-that distributes an archive to your web servers,
-using the function do_deploy
-"""
-
-from fabric.api import run, put, env
 import os
+from fabric.api import *
+
+# specify the hosts and run the command across the systems
+env.username = 'ubuntu'
+env.hosts = ['ubuntu@100.25.33.197', 'ubuntu@54.90.12.51']
+env.key_file = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-    """ Uncompresses and deploy the archive into the servers """
-
-    env.hosts = ['ubuntu@100.25.33.197', 'ubuntu@54.90.12.51']
-    if os.path.exists(archive_path) is False:
+    """Deploys the static files to the host servers.
+    Args:
+        archive_path (str): The path to the archived static files.
+    """
+    if not os.path.exists(archive_path):
         return False
-
-    data_path = '/data/web_static/releases/'
-    tmp = archive_path.split('.')[0]
-    name = tmp.split('/')[1]
-    dest = data_path + name
+    arch_file = os.path.basename(archive_path)
+    file_name = arch_file.replace(".tgz", "")
+    folder = "/data/web_static/releases/{}/".format(file_name)
+    status = False
 
     try:
-        put(archive_path, '/tmp')
-        run('sudo mkdir -p {}'.format(dest))
-        run('sudo tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
-        run('sudo rm -f /tmp/{}.tgz'.format(name))
-        run('sudo mv {}/web_static/* {}/'.format(dest, dest))
-        run('sudo rm -rf {}/web_static'.format(dest))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {} /data/web_static/current'.format(dest))
-        return True
+        # upload the archive to the /tmp/ directory of web server
+        put(archive_path, "/tmp/{}".format(file_name))
+
+        # create directory to extract archive into
+        run("mkdir -p {}".format(folder))
+
+        # uncompress the archive
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder))
+
+        # delete the archive from the web server
+        run("rm -rf /tmp/{}".format(file_name))
+
+        # copy all static files to created directory
+        run("mv {}web_static/* {}".format(folder, folder))
+
+        run("rm -rf {}web_static".format(folder))
+
+        # delete the symbolic link
+        run("rm -rf /data/web_static/current")
+
+        # create sybmolic link to static files directory
+        run("ln -s {} /data/web_static/current".format(folder))
+        print("New version deployed")
+        status = True
     except Exception:
-        return False
+        status = False
+    return status
