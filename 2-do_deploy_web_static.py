4d88d02704b982efb98bfe265da11a973e986a35
webstarly <edidionge.ukem@gmail.com>
2023-04-20 09:34:54 +0000

    clone_v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,7 +12,7 @@ import os
 def do_deploy(archive_path):
     """ Uncompresses and deploy the archive into the servers """
 
-    env.hosts = ['3.238.99.98', '34.207.154.242']
+    env.hosts = ['ubuntu@100.25.33.197', 'ubuntu@54.90.12.51']
     if os.path.exists(archive_path) is False:
         return False
 
